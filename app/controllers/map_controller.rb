0d2edb6ab6868acb7e51fa190f09d881928a0e1b
Jamal Powell <jamalkpowell@gmail.com>
2013-10-06 14:46:05 -0400

    modified notes file

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -5,17 +5,12 @@ class MapController < ApplicationController
 
   def tweets
     @tweets = Tweet.all
-    @lat = Tweet(params[:lat])
-    @lng = Tweet(params[:lng])
-    @lang = Tweet(params[:lang])
+    # @lat = Tweet.(params[:lat])
+    # @lng = Tweet(params[:lng])
+    # @lang = Tweet(params[:lang])
     respond_to do |format|
-<<<<<<< HEAD
     format.html
     format.json {render json: @tweets}
-=======
-      format.html
-      format.json {render json: @tweets}
->>>>>>> f262969e7fb394c3652e8ffca6193d1a3b26a186
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Babble::Application.routes.draw do
   get '/' => 'map#index'
-<<<<<<< HEAD
-  get '/index' => 'map#tweets'
-=======
   get '/tweets' => 'map#tweets'
->>>>>>> f262969e7fb394c3652e8ffca6193d1a3b26a186
 end
